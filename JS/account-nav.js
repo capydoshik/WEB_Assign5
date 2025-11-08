fdd2f1c06b16158b5373a211bf1fdfabb6f7f8c2
Dosh1K <ayazbaev.daniar@gmail.com>
2025-11-08 20:32:05 +0500

    account is fixed

--- JS/account-nav.js
+++ JS/account-nav.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const accountLink = document.querySelector('.header-nav a[href="../HTML/login.html"]');
+    const accountLink = document.querySelector('.header-nav a[href="HTML/login.html"]');
     const loggedIn = localStorage.getItem("loggedIn") === "true";
     const user = JSON.parse(localStorage.getItem("user"));
 
     if (loggedIn && user) {
         accountLink.textContent = "My Profile";
-        accountLink.href = "../HTML/profile.html";
+        accountLink.href = "HTML/profile.html";
     } else {
         accountLink.textContent = "Account";
-        accountLink.href = "../HTML/login.html";
+        accountLink.href = "HTML/login.html";
     }
 });
